163e826257824c055d39d276befb0b2ab165c46d
ikechukwu0395 <ikechukwumacdouglas@gmail.com>
2023-12-13 23:01:28 +0100

    shell_error_1.c

---
+++ shell_error_1.c
@@ -0,0 +1,79 @@
+#include "main.h"
+
+/**
+ * get_error - calls the error according the builtin,
+ * syntax or permission
+ * @datash: the data structure
+ * @eval: error value
+ * Return: error
+ */
+int get_error(data_shell *datash, int eval)
+{
+	char *error;
+
+	switch (eval)
+	{
+	case -1:
+		error = error_env(datash);
+		break;
+	case 126:
+		error = error_path_126(datash);
+		break;
+	case 127:
+		error = error_not_found(datash);
+		break;
+	case 2:
+		if (_strcmp("exit", datash->args[0]) == 0)
+			error = error_exit_shell(datash);
+		else if (_strcmp("cd", datash->args[0]) == 0)
+			error = error_get_cd(datash);
+		break;
+	}
+
+	if (error)
+	{
+		_printerr(error);
+		free(error);
+	}
+
+	datash->status = eval;
+	return (eval);
+}
+
+
+/**
+ * strcat_cd - function that concatenates the message
+ * @datash: data relevant
+ * @msg: message to print
+ * @error: output message
+ * @ver_str: counter lines
+ * Return: error message
+ */
+char *strcat_cd(data_shell *datash, char *msg, char *error, char *ver_str)
+{
+	char *illegal_flag;
+
+	_strcpy(error, datash->av[0]);
+	_strcat(error, ": ");
+	_strcat(error, ver_str);
+	_strcat(error, ": ");
+	_strcat(error, datash->args[0]);
+	_strcat(error, msg);
+	if (datash->args[1][0] == '-')
+	{
+		illegal_flag = malloc(3);
+		illegal_flag[0] = '-';
+		illegal_flag[1] = datash->args[1][1];
+		illegal_flag[2] = '\0';
+		_strcat(error, illegal_flag);
+		free(illegal_flag);
+	}
+	else
+	{
+		_strcat(error, datash->args[1]);
+	}
+
+	_strcat(error, "\n");
+	_strcat(error, "\0");
+	return (error);
+}
